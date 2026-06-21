8bd42fa85590975e710e49ad931ff581c25f4c24
Paratha-iscool <prarthna.isthebest@gmail.com>
2026-06-21 13:56:14 +0400

    Accidentally left Deleted EmotionsContext in

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+<<<<<<< Updated upstream
+=======
+import AgeChoice from './components/AgeChoice'
+import EmotionsChoice from './components/EmotionsChoice'
+import { useState } from 'react';
+>>>>>>> Stashed changes
 import Home from "./components/Home"
 
 function App() {
